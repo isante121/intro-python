f3a38f08e067b29728e8046fc345c3ae0a31b5cc
isante121 <158183785+isante121@users.noreply.github.com>
2024-01-31 16:22:24 +0000

    kick off assginment

---
+++ spring intro to python/kickoff warmup.py
@@ -0,0 +1,10 @@
+develop and test software and software adheres to best practices in performance, reliability and security. Computer programmers can work developing, coding video games, programming websites
+
+My favorite food is rice and chicken
+1:wash your rice. 
+2:boil a pot of water add lillte bit of salt.
+3:add rice to the boiling pot of water wait 25 mins.
+4:on a frying pan medium heat add butter or oil.
+5:add your chicken to the pan add whatever you want in the pan
+6:let the chicken cook of 15 mins on medium.
+7:add the rice and chicken together.
