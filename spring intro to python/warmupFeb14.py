1a9edea88c57d31dbb7474f2b6542b148b9963d1
isante121 <158183785+isante121@users.noreply.github.com>
2024-02-14 15:20:35 +0000

    work

---
+++ spring intro to python/warmupFeb14.py
@@ -0,0 +1,3 @@
+print(Any function that is provided as part of a high-level language and can be executed by a simple reference with specification of arguments)
+print(This function is used to print the type or the class the object passed as an argument belongs to. This function is used for debugging purposes.)
+print(Sequence list, tuple, range holds collection of items)
